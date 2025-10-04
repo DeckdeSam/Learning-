9b0b36ae8c75df17e61c704e805837a8a4522fd8
DeckdeSam <crsytpubg@gmail.com>
2025-10-04 09:43:54 +0530

    Added FirstDemo.java

---
+++ JavaGit/FirstDemo.java
@@ -0,0 +1,6 @@
+class sam
+{
+    public static void main(String[] args) {
+        System.out.println("Hii i am Sameer");
+    }
+}
---
+++ JavaGit/Motki.java
@@ -0,0 +1,14 @@
+class Sam
+{
+   static void Display()
+    {
+        System.out.println("Main Hun Sam");
+    }
+}
+
+public class Motki {
+    public static void main(String[] args) {
+
+        Sam.Display();
+    }
+}
